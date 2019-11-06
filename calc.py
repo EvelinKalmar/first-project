4a0d915b81ecb8bc029412eb65170dcacdc00fb5
EvelinKalmar <evelin.kalmar@pontsystems.eu>
2019-11-06 20:10:35 +0100

    Első commitom ever

--- calc.py
+++ calc.py
@@ -5,10 +5,10 @@ driver = webdriver.Chrome("C:\\Users\\kalmarevelin\\chromedriver.exe")
 #driver = webdriver.Chrome()
 driver.get("https://evelinkalmar.github.io/first-project/")
 driver.find_element(By.ID, "a-input").send_keys("1")
-driver.find_element (By.ID, "b-input").send_keys("1")
-driver.find_element (By.ID, "submit-button").click()
-result = driver.find_element (By.ID, "result-input").get_attribute("value")
-print (result)
+driver.find_element(By.ID, "b-input").send_keys("1")
+driver.find_element(By.ID, "submit-button").click()
+result = driver.find_element(By.ID, "result-input").get_attribute("value")
+print(result)
 assert result == "2"
 
 
