b51cf054e90efe0b7592de688c18d84885dda12b
EvelinKalmar <evelin.kalmar@pontsystems.eu>
2019-11-06 20:07:53 +0100

    Első commitom ever

--- calc.py
+++ calc.py
@@ -4,13 +4,19 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome("C:\\Users\\kalmarevelin\\chromedriver.exe")
 #driver = webdriver.Chrome()
 driver.get("https://evelinkalmar.github.io/first-project/")
-driver.find_element(By.NAME, "a").send_keys("1")
-driver.find_element (By.NAME, "b").send_keys("1")
+driver.find_element(By.ID, "a-input").send_keys("1")
+driver.find_element (By.ID, "b-input").send_keys("1")
+driver.find_element (By.ID, "submit-button").click()
+result = driver.find_element (By.ID, "result-input").get_attribute("value")
+print (result)
+assert result == "2"
 
-header_text = driver.find_element(By.XPATH, "//h1").text
+
+
+header_text = driver.find_element(By.ID, "//h1").text
 print (header_text)
 assert header_text == "Számológép"
-driver.find_element (By.XPATH, "//input[3]").click
+
 
 
 
