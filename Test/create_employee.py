052ff5eefc40807039fc92fd9fce0d95e2b6cc14
kalmarevelin <evelin.kalmar@pontsystems.eu>
2019-11-20 18:30:16 +0100

    5. Órai munka

--- Test/create_employee.py
+++ Test/create_employee.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 chrome_options = webdriver.ChromeOptions();
 chrome_options.add_experimental_option("excludeSwitches",
@@ -11,6 +13,10 @@ driver.get("http://www.learnwebservices.com/empapp/employees.xhtml")
 create = driver.find_element(By.XPATH, "/html/body/div/a").click()
 input_name = driver.find_element(By.ID, "create-form:name-input").click()
 title = driver.find_element(By.XPATH, "//body/div/h1").click()
+
+WebDriverWait(driver, 10).until(
+    expected_conditions.text_to_be_present_in_element((By.XPATH, "//span[@class = 'invalid-feedback']"),
+                                                      "Az alkalmazott nevét meg kell adni!"))
 message = driver.find_element(By.XPATH, "//span[@class = 'invalid-feedback']").text
 print(message)
 
